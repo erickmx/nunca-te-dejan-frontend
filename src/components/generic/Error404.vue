e6f2246d00fd6db7d80c34775ac18bfe4ef5a523
erickmx <erickblanc96@gmail.com>
2017-11-10 02:17:56 -0600

    creacion del compponente 404

--- src/components/generic/Error404.vue
+++ src/components/generic/Error404.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    Error component
-  </div>
+
+  <v-container fill-height>
+    <v-layout row wrap align-center>
+      <v-flex class="text-xs-center">
+        <img :src="src" alt="page not found" width="70%" height="70%">
+      </v-flex>
+    </v-layout>
+  </v-container>
+  
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      src: '/static/repair.jpg'
+    }
+  }
 }
 </script>
 
